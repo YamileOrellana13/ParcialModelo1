9384d602a36059475b2e284c55d84d51bd24fc11
yamileandreaorellanasejas@gmail.com <yamileandreaorellanasejas@gmail.com>
2019-04-05 22:46:21 -0400

    Commit Con Modelo  con MVC

---
+++ parcial/MVCparcial/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCparcial.Models
+{
+    public class DataContext:DbContext         //DbContex doy Ctrl .
+    {
+        public DataContext():base("DefaultConnection")        // doy ctor tab tab y me sale todo
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<MVCparcial.Models.Product> Products { get; set; }
+    }
+}
---
+++ parcial/MVCparcial/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MVCparcial.Models
+{
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+    public enum TipoEstado
+    {
+        Activo, 
+        Inactivo
+    }
+    public class Product
+    {
+        [Key]
+        public int ProdctoId { get; set; }  //prop+tab tab
+        [Required]
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        [Required]
+        public TipoUnidad Unidad { get; set; }
+        public TipoEstado Estado { get; set; }
+
+    }
+}
