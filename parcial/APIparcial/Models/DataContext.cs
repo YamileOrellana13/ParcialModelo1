25edd37a15f0742cfaa55f5c04fa9266236c5d59
yamileandreaorellanasejas@gmail.com <yamileandreaorellanasejas@gmail.com>
2019-04-05 23:22:20 -0400

    Commit con Modelo Web Api

---
+++ parcial/APIparcial/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace APIparcial.Models
+{
+    public class DataContext:DbContext         //DbContex doy Ctrl .
+    {
+        public DataContext():base("DefaultConnection")        // doy ctor tab tab y me sale todo
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<APIparcial.Models.Product> Products { get; set; }
+    }
+}
